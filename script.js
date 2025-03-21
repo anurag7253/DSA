cb424a6b17c01a34978922da73e643143d9f0bc3
Anurag Sharma <anuragsaitiya45@gmail.com>
2025-03-21 11:12:05 +0530

    re fix

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+// map 
+function initMap() {
+    var location = { lat: -25.344, lng: 131.036 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
